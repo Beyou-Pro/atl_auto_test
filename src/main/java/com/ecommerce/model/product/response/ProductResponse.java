3dbed415845ad522ea2ce0cfc1194c6e4dee7338
Beyou-Pro <159903460+Beyou-Pro@users.noreply.github.com>
2025-07-07 14:56:45 +0200

    [FIX] fix product description

--- src/main/java/com/ecommerce/model/product/response/ProductResponse.java
+++ src/main/java/com/ecommerce/model/product/response/ProductResponse.java
@@ -1,7 +1,9 @@
 package com.ecommerce.model.product.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
-public record ProductResponse(String id, String name, String descriptionShort, String descriptionLong, List<String> images,
+public record ProductResponse(String id, String name, @JsonProperty("description_short") String descriptionShort, @JsonProperty("description_long") String descriptionLong, List<String> images,
                               double price, int stock) {
 }
