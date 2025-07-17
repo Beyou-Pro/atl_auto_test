784d7b4378a8adff1557f2c5e45ad3b2d88b6d3c
Beyou <159903460+Beyou-Pro@users.noreply.github.com>
2025-07-17 11:30:04 +0200

    [ADD] customer service

---
+++ src/main/java/com/ecommerce/service/customer/impl/CustomerServiceImpl.java
@@ -0,0 +1,33 @@
+package com.ecommerce.service.customer.impl;
+
+import com.ecommerce.entity.customer.Customer;
+import com.ecommerce.repository.customer.CustomerRepository;
+import com.ecommerce.service.customer.CustomerService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Transactional
+    @Override
+    public Customer getOrCreateCustomer(UUID customerId) {
+        return customerRepository.findById(customerId)
+                .orElseGet(() -> {
+                    Customer guest = new Customer();
+                    guest.setId(customerId);
+                    return customerRepository.save(guest);
+                });
+    }
+
+}
